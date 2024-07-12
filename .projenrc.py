eb0d16c0285f42d64cf4722bc285ad0081d42b01
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-07-12 18:36:25 +0300

    deps: new dependencies

--- .projenrc.py
+++ .projenrc.py
@@ -1,3 +1,4 @@
+from projen import ProjectType
 from projen.python import PythonProject
 
 project = PythonProject(
@@ -6,8 +7,14 @@ project = PythonProject(
     module_name="wrapper",
     name="function-python",
     version="0.1.0",
+    github=True,
+    project_type=ProjectType.APP,
+    poetry=False,
+    pytest=False,
 )
 project.add_git_ignore(".mise.toml")
-project.add_dependency("rocketmq-client-python==2.0.0")
+project.add_git_ignore(".secrets.yaml")
+project.add_dependency("nats-py==2.8.0")
 project.add_dependency("aws-lambda-typing==2.20.0")
+project.add_dependency("dynaconf==3.2.5")
 project.synth()
